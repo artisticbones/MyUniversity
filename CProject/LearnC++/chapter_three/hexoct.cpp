b49d3762d208276af6a999ffd536d5a6d75ed475
Cheng Feitian <artisticbones@163.com>
2020-04-16 22:37:44 +0800

    update

---
+++ CProject/LearnC++/chapter_three/hexoct.cpp
@@ -0,0 +1,21 @@
+/** 
+ * Author@ Cheng Feitian
+ * Date: 2020-4-16
+ * Function: show hex and octal literals
+ */
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int chest = 42;
+    int waist = 0x42;
+    int inseam = 042;
+
+    cout << "Monsieur cuts a striking figure!" << endl;
+    cout << "chest = " << chest << " (42 in decimal)" << endl;
+    cout << "waist = " << waist << " (0x42 in hex)" << endl;
+    cout << "inseam = " << inseam << " (042 in octal)" << endl;
+    return 0;
+}
