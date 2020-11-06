a33d6cca66e7dcbb8f645280745a73afa495dd6d
artisticbones <artisticbones@163.com>
2020-11-06 23:13:47 +0800

    finished SingleLinkList

--- PostGradute/DataStructure/chapter_two/SingleList.cpp
+++ PostGradute/DataStructure/chapter_two/SingleList.cpp
@@ -69,7 +69,7 @@ LNode *GetElem(LinkList L,int i){
     }
     if (i < 1)
     {
-        return NULL                     //若i无效，则返回NULL
+        return NULL;                    //若i无效，则返回NULL
     }
     while (p && j < i)                  //从第1个节点开始找，查找第i个节点
     {
@@ -90,7 +90,8 @@ LNode *LocateElem(LinkList L,Elemtype e){
     return p;
 }
 
-LinkList LinkInsert(LinkInsert &L,int i,Elemtype e){
+LinkList LNodeInsert(LinkList &L,int i,Elemtype e)
+{
     LNode *p,*s;
     p = GetElem(L,i - 1);
     s = (LNode*)malloc(sizeof(LNode));
@@ -98,7 +99,44 @@ LinkList LinkInsert(LinkInsert &L,int i,Elemtype e){
     s->next = p->next;
     p->next = s;
 
+    /* 前插法 */
+    // s->next = p->next;
+    // p->next = s;
+    // temp = p->data;
+    // p->data = s->data;
+    // s->data = temp;
+
     return L;
+}
+
+LinkList LNodeDelete(LinkList &L,int i)
+{
+    LNode *p,*q;
+    p = GetElem(L,i - 1);               //查找删除位置的前驱节点
+    q = p->next;                        //令q指向被删除节点
+    p->next = q->next;                  //将*q节点从链中断开
+    free(q);                            //释放节点存储空间
+    
+    /* 删除节点*p的操作可用删除*p的后继节点操作来实现，实质就是将其后继节点的值赋予其自身，然后删除后继节点，并将时间复杂度变为O(1) */
+    // q = p->next;                        //令q指向*p的后继节点
+    // p->data = p->next->data;            //和后继节点交换数据域
+    // p->next = q->next;                  //将*q节点从链中断开
+    // free(q);
+    return L;
+}
+
+int GetListLength(LinkList L)
+{
+    LNode *p;
+    int count = 0;
+
+    p = L->next;
+
+    while (p->next != NULL)
+    {
+       count++; 
+    }
+    return count;
 }
 
 int main() {
@@ -106,11 +144,50 @@ int main() {
     freopen("./result/1.in", "r", stdin);
     freopen("./result/1.out", "w", stdout);
 #endif
-
-
-
-
-
+    LinkList L;
+    int condition;
+    
+    do{
+        cout << "Please choose the function:";
+        cin >> condition;
+        if (condition == 1)
+        {
+            if(List_HeadInsert(L)){
+                cout << "Success!" << endl;
+            }
+            else
+            {
+                cout << "Insert false!" << endl;
+            }
+            
+           
+        }
+        else if (condition == 2)
+        {
+            int location = 0;
+            cout << "Please input location:" << endl;
+            cin >> location;
+            LNodeDelete(L,location);
+           
+        }
+        else if (condition == 3)
+        {
+            Elemtype e;
+            cout << "Please input the value you want to locate:";
+            cin >> e;
+            cout << "The Location is: " << LocateElem(L,e) << endl;
+        }
+        else if (condition == 5)
+        {
+            for (LNode *p = L->next;p->next != NULL; p = p->next)
+            {
+                cout << p->data << "\t";
+            }
+            
+            cout << endl;               
+        }
+        
+    }while (condition != 10000);
 
     return 0;
 }
