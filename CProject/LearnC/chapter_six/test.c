17bebd4f2435d65560421ff87f075d8c2bb059ad
Cheng Feitian <artisticbones@163.com>
2021-02-06 21:44:52 +0800

    sth need push

---
+++ CProject/LearnC/chapter_six/test.c
@@ -0,0 +1,20 @@
+/**
+ * Author@ Cheng Feitian
+ * Date: 2020-2-6
+ * Description: 测试c99类型
+ * Input Format: None
+ * Output Format: None
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#define MAXSIZE 1024
+
+int main(void) {
+    /* 初始化 */
+    
+    /* 主逻辑 */
+
+    return 0;
+}
